30feca56e7f09890317c263061734bb5327f1298
star10919 <star10919@naver.com>
2021-07-28 16:43:35 +0900

    21-07-28

--- keras/keras59_5_men_women.py
+++ keras/keras59_5_men_women.py
@@ -6,83 +6,107 @@ import numpy as np
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from icecream import ic
 
-'''
-## ImageDataGenerator로 데이터 증폭시키기
-imageGen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    rotation_range=5,
-    zoom_range=1.2,
-    shear_range=0.7,
-    fill_mode='nearest',
-    validation_split=0.2
-    )
+
+# ## ImageDataGenerator로 데이터 증폭시키기
+# imageGen = ImageDataGenerator(
+#     rescale=1./255,
+#     horizontal_flip=True,
+#     vertical_flip=True,
+#     width_shift_range=0.1,
+#     height_shift_range=0.1,
+#     rotation_range=5,
+#     zoom_range=1.2,
+#     shear_range=0.7,
+#     fill_mode='nearest',
+#     validation_split=0.2
+#     )
 # test_datagen = ImageDataGenerator(rescale=1./255)
 
 
 
-trainGen = imageGen.flow_from_directory(
-    '../data/men_women',
-    target_size=(150, 150),
-    batch_size=2000,
-    subset='training'
-)
-# Found 2648 images belonging to 2 classes.
-ic(trainGen[0][0].shape)     # (2000, 150, 150, 3)
-ic(trainGen[0][1].shape)     # (2000, 2)
+# trainGen = imageGen.flow_from_directory(
+#     '../data/men_women',
+#     target_size=(150, 150),
+#     batch_size=2650,
+#     class_mode='binary',
+#     subset='training',
+#     shuffle=False
+# )
+# # Found 2649 images belonging to 2 classes.
+# ic(trainGen[0][0].shape)     # (2649, 150, 150, 3)
+# ic(trainGen[0][1].shape)     # (2649,)
 
 
-testGen = imageGen.flow_from_directory(
-    '../data/men_women',
-    target_size=(150, 150),
-    batch_size=1000,
-    subset='validation'
-)
-#Found 661 images belonging to 2 classes.
-ic(testGen[0][0].shape)     # (661, 150, 150, 3)
-ic(testGen[0][1].shape)     # (661, 2)
+# testGen = imageGen.flow_from_directory(
+#     '../data/men_women',
+#     target_size=(150, 150),
+#     batch_size=1000,
+#     class_mode='binary',
+#     subset='validation',
+#     shuffle=False
+# )
+# #Found 661 images belonging to 2 classes.
+# ic(testGen[0][0].shape)     # (661, 150, 150, 3)
+# ic(testGen[0][1].shape)     # (661,)
 
 
-# 넘파이로 저장
-np.save('./_save/_npy/k59_5_train_x.npy', arr=trainGen[0][0])
-np.save('./_save/_npy/k59_5_train_y.npy', arr=trainGen[0][1])
-np.save('./_save/_npy/k59_5_test_x.npy', arr=testGen[0][0])
-np.save('./_save/_npy/k59_5_test_y.npy', arr=testGen[0][1])
-'''
+# # 넘파이로 저장
+# np.save('./_save/_npy/k59_5_train_x.npy', arr=trainGen[0][0])
+# np.save('./_save/_npy/k59_5_train_y.npy', arr=trainGen[0][1])
+# np.save('./_save/_npy/k59_5_test_x.npy', arr=testGen[0][0])
+# np.save('./_save/_npy/k59_5_test_y.npy', arr=testGen[0][1])
 
-#============================================================================
+
+# #============================================================================
 
 x_train = np.load('./_save/_npy/k59_5_train_x.npy')
 y_train = np.load('./_save/_npy/k59_5_train_y.npy')
 x_test = np.load('./_save/_npy/k59_5_test_x.npy')
 y_test = np.load('./_save/_npy/k59_5_test_y.npy')
 
-# ic(x_train.shape, y_train.shape)    # (2000, 150, 150, 3), y_train.shape: (2000, 2)
-# ic(x_test.shape, y_test.shape)      # (661, 150, 150, 3), y_test.shape: (661, 2)
+# ic(x_train.shape, y_train.shape)    # (2649, 150, 150, 3), y_train.shape: (2649,)
+# ic(x_test.shape, y_test.shape)      # (661, 150, 150, 3), y_test.shape: (661,)
+
+x_train = x_train[:-1,:,:,:]
+x_pred = x_train[-1,:,:,:].reshape(1,150,150,3)
+y_train = y_train[:-1]
+# y_pred = y_train[-1]
+ic(x_train.shape, y_train.shape)      # x_train.shape: (2648, 150, 150, 3), y_train.shape: (2648,)
+ic(x_pred.shape)                      # x_pred.shape: (1, 150, 150, 3)
+
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropout
 
 model = Sequential()
-model.add(Conv2D(32, (2,2), padding='same', input_shape=(150,150,3)))
-model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
-model.add(Dropout(rate=0.3))
+model.add(Conv2D(filters=32, kernel_size=(3,3), input_shape =(150,150,3), activation= 'relu'))
+model.add(Conv2D(filters=32, kernel_size=(3,3), activation= 'relu'))
+model.add(Conv2D(filters=64, kernel_size=(2,2), activation= 'relu'))
+model.add(Conv2D(filters=64, kernel_size=(3,3), activation= 'relu'))
 model.add(Flatten())
-model.add(Dense(32, activation='relu'))
-model.add(Dense(32, activation='relu'))
-model.add(Dense(16, activation='relu'))
-model.add(Dense(2, activation='sigmoid'))
+model.add(Dense(128, activation= 'relu'))
+model.add(Dense(64, activation= 'relu'))
+model.add(Dense(1, activation= 'sigmoid'))
 
 
 # 3. 컴파일(ES), 훈련
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
 
-hist = model.fit(x_train, y_train, epochs=50, steps_per_epoch=32, validation_steps=4)
+from tensorflow.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_;oss', patience=10, mode='min', verbose=1)
+hist = model.fit(x_train, y_train, epochs=50, steps_per_epoch=32, validation_steps=4, callbacks=[es])
 
 
 results = model.evaluate(x_test, y_test)
 print('binary :', results[0])
 print('acc :', results[1])
+
+y_predict = model.predict(x_pred)
+res = y_predict * 100
+print('여자일 확률 :', res, '%')
+
+'''
+binary : 3.5364973545074463
+acc : 0.5854765772819519
+여자일 확률 : [[100.]] %
+'''
--- keras/keras59_6_rps.py
+++ keras/keras59_6_rps.py
@@ -7,55 +7,55 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from icecream import ic
 
 
-## ImageDataGenerator로 데이터 증폭시키기
-imageGen = ImageDataGenerator(
-    rescale=1./255,
-    horizontal_flip=True,
-    vertical_flip=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    rotation_range=5,
-    zoom_range=1.2,
-    shear_range=0.7,
-    fill_mode='nearest',
-    validation_split=0.2
-    )
-# test_datagen = ImageDataGenerator(rescale=1./255)
+# ## ImageDataGenerator로 데이터 증폭시키기
+# imageGen = ImageDataGenerator(
+#     rescale=1./255,
+#     horizontal_flip=True,
+#     vertical_flip=True,
+#     width_shift_range=0.1,
+#     height_shift_range=0.1,
+#     rotation_range=5,
+#     zoom_range=1.2,
+#     shear_range=0.7,
+#     fill_mode='nearest',
+#     validation_split=0.2
+#     )
+# # test_datagen = ImageDataGenerator(rescale=1./255)
 
 
 
 
-trainGen = imageGen.flow_from_directory(
-    '../data/rps',
-    target_size=(150, 150),
-    batch_size=2000,
-    subset='training'
-)
-# Found 2016 images belonging to 3 classes.
-ic(trainGen[0][0].shape)     # trainGen[0][0].shape: (2000, 150, 150, 3)
-ic(trainGen[0][1].shape)     # trainGen[0][1].shape: (2000, 3)
+# trainGen = imageGen.flow_from_directory(
+#     '../data/rps',
+#     target_size=(150, 150),
+#     batch_size=2000,
+#     subset='training'
+# )
+# # Found 2016 images belonging to 3 classes.
+# ic(trainGen[0][0].shape)     # trainGen[0][0].shape: (2000, 150, 150, 3)
+# ic(trainGen[0][1].shape)     # trainGen[0][1].shape: (2000, 3)
 
 
-testGen = imageGen.flow_from_directory(
-    '../data/rps',
-    target_size=(150, 150),
-    batch_size=1000,
-    subset='validation'
-)
-# Found 504 images belonging to 3 classes.
-ic(testGen[0][0].shape)     # testGen[0][0].shape: (504, 150, 150, 3)
-ic(testGen[0][1].shape)     # testGen[0][1].shape: (504, 3)
+# testGen = imageGen.flow_from_directory(
+#     '../data/rps',
+#     target_size=(150, 150),
+#     batch_size=1000,
+#     subset='validation'
+# )
+# # Found 504 images belonging to 3 classes.
+# ic(testGen[0][0].shape)     # testGen[0][0].shape: (504, 150, 150, 3)
+# ic(testGen[0][1].shape)     # testGen[0][1].shape: (504, 3)
 
 
-# 넘파이로 저장
-np.save('./_save/_npy/k59_6_train_x.npy', arr=trainGen[0][0])
-np.save('./_save/_npy/k59_6_train_y.npy', arr=trainGen[0][1])
-np.save('./_save/_npy/k59_6_test_x.npy', arr=testGen[0][0])
-np.save('./_save/_npy/k59_6_test_y.npy', arr=testGen[0][1])
+# # 넘파이로 저장
+# np.save('./_save/_npy/k59_6_train_x.npy', arr=trainGen[0][0])
+# np.save('./_save/_npy/k59_6_train_y.npy', arr=trainGen[0][1])
+# np.save('./_save/_npy/k59_6_test_x.npy', arr=testGen[0][0])
+# np.save('./_save/_npy/k59_6_test_y.npy', arr=testGen[0][1])
 
 
 #============================================================================
-'''
+
 x_train = np.load('./_save/_npy/k59_6_train_x.npy')
 y_train = np.load('./_save/_npy/k59_6_train_y.npy')
 x_test = np.load('./_save/_npy/k59_6_test_x.npy')
@@ -70,16 +70,20 @@ from tensorflow.keras.layers import Dense, Conv2D, Flatten, MaxPooling2D, Dropou
 model = Sequential()
 model.add(Conv2D(32, (2,2), padding='same', input_shape=(150,150,3)))
 model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(2,2))
+model.add(Dropout(rate=0.3))
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(2,2))
 model.add(Dropout(rate=0.3))
 model.add(Flatten())
 model.add(Dense(32, activation='relu'))
 model.add(Dense(32, activation='relu'))
 model.add(Dense(16, activation='relu'))
-model.add(Dense(2, activation='sigmoid'))
+model.add(Dense(3, activation='softmax'))
 
 
 # 3. 컴파일(ES), 훈련
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['acc'])
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
 
 hist = model.fit(x_train, y_train, epochs=50, steps_per_epoch=32, validation_steps=4)
 
@@ -87,4 +91,3 @@ hist = model.fit(x_train, y_train, epochs=50, steps_per_epoch=32, validation_ste
 results = model.evaluate(x_test, y_test)
 print('binary :', results[0])
 print('acc :', results[1])
-'''
