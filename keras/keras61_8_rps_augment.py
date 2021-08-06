89108674f227bdb540b286bb00807598f881f090
star10919 <star10919@naver.com>
2021-08-06 15:56:10 +0900

    21-08-06

--- keras/keras61_8_rps_augment.py
+++ keras/keras61_8_rps_augment.py
@@ -67,7 +67,7 @@ x_train = np.load('./_save/_npy/k59_6_train_x.npy')
 y_train = np.load('./_save/_npy/k59_6_train_y.npy')
 x_test = np.load('./_save/_npy/k59_6_test_x.npy')
 y_test = np.load('./_save/_npy/k59_6_test_y.npy')
-
+print('****************** 1 *****************')
 ic(x_train.shape, y_train.shape)    # (2000, 150, 150, 3), y_train.shape: (2000, 3)
 ic(x_test.shape, y_test.shape)      # (504, 150, 150, 3), y_test.shape: (504, 3)
 
