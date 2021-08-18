e794973ab449e01f3918243b40fbab30ead3d37b
star10919 <star10919@naver.com>
2021-08-18 15:07:20 +0900

    21-08-18

--- keras2/keras64_hyperParameter.py
+++ keras2/keras64_hyperParameter.py
@@ -31,9 +31,9 @@ def build_model(drop=0.5, optimizer='adam'):
     return model
 
 def create_hyperparameter():
-    batches = [10, 20, 30, 40, 50]
+    batches = [1000, 2000, 3000, 4000, 5000]
     optimizer = ['rmsprop', 'adam', 'adadelta']
-    dropout = [0.1, 0.2, 0.3]
+    dropout = [0.3, 0.4, 0.5]
     return {"batch_size" : batches, "optimizer": optimizer, "drop" : dropout}
 
 
@@ -44,12 +44,25 @@ hyperparameters = create_hyperparameter()
 
 ###############################################################################################################################
 from tensorflow.keras.wrappers.scikit_learn import KerasClassifier, KerasRegressor  # 사이킷런으로 케라스(텐서플로우)모델 감쌈
-model2 = KerasClassifier(build_fn=build_model, verbose=1)
+model2 = KerasClassifier(build_fn=build_model, verbose=1, validation_split=0.2) #, epochs=2)  # ***epochs는 2군데 먹힘-1, validation_split도 2군데 먹힘
 ###############################################################################################################################
 
 
 from sklearn.model_selection import GridSearchCV, RandomizedSearchCV
-model = RandomizedSearchCV(model2, hyperparameters, cv=5)        # (모델, 하파튜, 크로스발리데이션)
+# model = RandomizedSearchCV(model2, hyperparameters, cv=5)        # (모델, 하파튜, 크로스발리데이션) 
                             #텐서플로우 모델이라 에러뜸 -> 사이킷런으로 텐서플로우 모델 래핑해야 함!
+model = GridSearchCV(model2, hyperparameters, cv=2)   
 
-model.fit(x_train, y_train, verbose=1)
+model.fit(x_train, y_train, verbose=1, epochs=3)#, validation_split=0.2)  # ***epochs는 2군데 먹힘-2  /  둘이 같이 주면 아래꺼가 먹힘 /  validation_split도 2군데 먹힘
+
+print(model.best_estimator_)
+print(model.best_params_)
+print(model.best_score_)
+acc = model.score(x_test, y_test)
+print("최종 스코어 :", acc)
+
+'''
+{'batch_size': 1000, 'drop': 0.3, 'optimizer': 'adam'}
+0.9412000179290771
+최종 스코어 : 0.9631999731063843
+'''
