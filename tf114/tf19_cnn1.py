0fe02fca6cdcf1c5bc5bb64582cf6e8c4f707672
star10919 <star10919@naver.com>
2021-08-30 12:22:21 +0900

    21-08-30

---
+++ tf114/tf19_cnn1.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+import numpy as np
+from keras.models import Sequential
+from keras.layers import Conv2D
+
+tf.set_random_seed(66)
+
+### get_variable vs Variable
+### CNN
+
+
+# 1. 데이터
+from keras.datasets import mnist
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+from keras.utils import to_categorical
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+x_train = x_train.reshape(60000, 28, 28, 1).astype('float32')/255.
+x_test = x_test.reshape(10000, 28, 28, 1).astype('float32')/255.
+
+learning_rate = 0.001
+training_epochs = 15
+batch_size = 100
+total_batch = int(len(x_train)/batch_size)
+
+x = tf.placeholder(tf.float32, [None, 28, 28, 1])
+y = tf.placeholder(tf.float32, [None, 10])
+
+# 모델구성
+
+# get_variable(통상적으로 더 많이 쓰임) vs Variable  -  차이는 없으나 사용할 때 shape이 다름
+# w1 = tf.get_variable('w1', shape=[3, 3,  1, 32])        # get_variable : 초기값 지정하지 않아도 자동으로 지정됨!!! / 네임, shape은 꼭 넣어 줘야 함
+# w1 = tf.Variable(tf.random_normal([3, 3, 1, 32]),dtype=tf.float32)      # Variable : 초기값 지정 반드시 해줘야 함!!!
+# w1 = tf.Variable([1], dtype = tf.float32)
+
+# sess = tf.Session()
+# sess.run(tf.global_variables_initializer())
+# print(np.min(sess.run(w1)))
+# print('==========================')
+# print(np.max(sess.run(w1)))
+# print('==========================')
+# print(np.mean(sess.run(w1)))
+# print('==========================')
+# print(np.median(sess.run(w1)))
+# print('==========================')
+
+# 변수 초기화
+# tensorflow 1
+w1 = tf.get_variable('w1', shape=[3, 3, 1, 32])   # [kernel_size, input의 channel(color) 수(맨 마지막), output]
+layer1 = tf.nn.conv2d(x, w1, strides=[1,1,1,1], padding='SAME')   # x, w의 차원이 같아야 함    # strides : 4차원으로 잡아줘야 함
+
+# tensorflow 2
+# model = Sequential()
+# model.add(Conv2D(filters=32, kernel_size=(3,3), strides=1, padding='same', input_shape=(28, 28, 1)))
+
+print(w1)       # (3, 3, 1, 32)
+print(layer1)   # (?, 28, 28, 32)
+
+
+
+
+# print(sess.run(w1))
+# print(w1)
+
+
