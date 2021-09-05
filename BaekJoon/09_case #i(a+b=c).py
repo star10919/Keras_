f647375973b52b226f32696c469c563b51834d57
star10919 <star10919@naver.com>
2021-09-06 00:26:01 +0900

    21-09-06

---
+++ BaekJoon/09_case #i(a+b=c).py
@@ -0,0 +1,10 @@
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+# 각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+
+T = int(input())
+
+for i in range(1, T+1):
+    line = input()
+    a = line.split(" ")[0]
+    b = line.split(" ")[1]
+    # print(f'Case #{i}: {a}, '+', {b}, '=', {int(a) + int(b)}')        # 에러남
