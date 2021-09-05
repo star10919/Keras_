5e289879fe30e36460282d46a5bb0a736b14b0ec
star10919 <star10919@naver.com>
2021-09-05 15:16:28 +0900

    21-09-05

--- BaekJoon/1_구구단.py
+++ BaekJoon/1_구구단.py
@@ -2,13 +2,10 @@
 # 첫째 줄에 N이 주어진다. N은 1보다 크거나 같고, 9보다 작거나 같다.
 # 출력형식과 같게 N*1부터 N*9까지 출력한다.
 
-import numpy as np
 
-gugu = None
-gu_list = []
+gugu = input()
 
 for i in range(1, 10):
-    gugu = print("n단 입력 :", input(gugu))
-    gugudan = gugu * i
-    gu_list.append(gugudan)
-    print(gu_list)
+    gugudan = int(gugu) * i
+
+    print(gugu,'*', i, '=', gugudan)
---
+++ BaekJoon/2_A+B.py
@@ -0,0 +1,12 @@
+# 두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+# 첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+# 각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+# 각 테스트 케이스마다 A+B를 출력한다.
+
+T = int(input())
+
+for i in range(1, T+1):
+    line = input()
+    a = line.split(" ")[0]
+    b = line.split(" ")[1]
+    print(int(a) + int(b))
