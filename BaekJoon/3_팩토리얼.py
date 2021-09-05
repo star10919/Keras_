22c334d6b95926be8524fd101ce6abca0978b8f2
star10919 <star10919@naver.com>
2021-09-05 16:27:39 +0900

    21-09-05

--- BaekJoon/3_팩토리얼.py
+++ BaekJoon/3_팩토리얼.py
@@ -2,7 +2,19 @@
 # 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
 # 1부터 n까지 합을 출력한다.
 
-n = input()
+# n = int(input())
+# result = 0
 
-for i in range(1, n+1):
-    sum = 
+# for i in range(1, n+1):
+#     result += i
+# print(result)
+
+# print(list(range(3)))
+
+input2 = int(input())
+list = []
+
+for i in range(1, input2+1):
+    list.append(i)
+
+print(sum(list))
