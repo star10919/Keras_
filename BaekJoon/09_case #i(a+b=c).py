9a9e3c7b9295c81b29e03f57811716ed422685aa
star10919 <star10919@naver.com>
2021-09-06 16:09:26 +0900

    21-09-06

--- BaekJoon/09_case #i(a+b=c).py
+++ BaekJoon/09_case #i(a+b=c).py
@@ -7,4 +7,4 @@ for i in range(1, T+1):
     line = input()
     a = line.split(" ")[0]
     b = line.split(" ")[1]
-    # print(f'Case #{i}: {a}, '+', {b}, '=', {int(a) + int(b)}')        # 에러남 
+    # print(f'Case #{i}: {a}, '+', {b}, '=', {int(a) + int(b)}')        # 에러남
