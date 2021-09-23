c39272659f858a2be792810160afb093201826bb
star10919 <star10919@naver.com>
2021-09-23 10:19:39 +0900

    21-09-23

--- 02_keras2/keras76_autokeras1.py
+++ 02_keras2/keras76_autokeras1.py
@@ -1,3 +1,5 @@
+# pip install autokeras  /  python 3.8.8  / tensorflow 2.4.1
+
 import autokeras as ak
 
 from tensorflow.keras.datasets import mnist
@@ -7,7 +9,7 @@ from tensorflow.keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 # 2. 모델
-model = ak.ImageClassifier(overwrite=True, max_trials=1)        # max_trials : 모델을 2번 돌리겠다.
+model = ak.ImageClassifier(overwrite=True, max_trials=1)        # max_trials : 모델을 2번 돌리겠다.   # validation default:0.2
 
 # 3. 훈련
 model.fit(x_train, y_train, epochs=2)
