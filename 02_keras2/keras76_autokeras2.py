4bf97e82721ca29526dca4574e3f228aa8ae6727
star10919 <star10919@naver.com>
2021-09-23 10:50:00 +0900

    21-09-23

---
+++ 02_keras2/keras76_autokeras2.py
@@ -0,0 +1,32 @@
+# pip install autokeras  /  python 3.8.8  / tensorflow 2.4.1
+
+import autokeras as ak
+
+from tensorflow.keras.datasets import mnist
+
+
+
+
+# 1. 데이터
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+
+# 2. 모델
+model = ak.ImageClassifier(overwrite=True, max_trials=1)        # max_trials : 모델을 2번 돌리겠다.   # validation default:0.2
+
+# model.sumary()     # summary 제공 X
+
+
+# 3. 훈련
+model.fit(x_train, y_train, epochs=2)
+
+
+# 4. 평가, 예측
+y_predict = model.predict(x_test)
+
+results = model.evaluate(x_test, y_test)
+print(results)
+
+
+model2 = model.export_model()
+model2.summary()
