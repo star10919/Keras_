9c96643c1e856b7af98e5e34f88cf6aea0a96394
star10919 <star10919@naver.com>
2021-09-23 11:21:49 +0900

    21-09-23

--- 02_keras2/keras76_autokeras3_AutoModel.py
+++ 02_keras2/keras76_autokeras3_AutoModel.py
@@ -1,13 +1,13 @@
+from sklearn.preprocessing.data import normalize
 import autokeras as ak
-
 from tensorflow.keras.datasets import mnist
-
 
 # 1. 데이터
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 x_train = x_train.reshape(-1, 28 * 28 * 1)
 x_test = x_test.reshape(-1, 28 * 28 * 1)
+
 print(x_train.shape, x_test.shape)
 
 from sklearn.preprocessing import MinMaxScaler
@@ -18,18 +18,29 @@ x_test = scaler.transform(x_test)
 x_train = x_train.reshape(-1, 28, 28, 1)
 x_test = x_test.reshape(-1, 28, 28, 1)
 
+
 from tensorflow.keras.utils import to_categorical
 y_train = to_categorical(y_train)
 y_test = to_categorical(y_test)
 
-
 # # 2. 모델
-model = ak.ImageClassifier(overwrite=True, max_trials=1)        # max_trials : 모델을 2번 돌리겠다.
+# model = ak.ImageClassifier(overwrite=True, max_trials=1)        # max_trials : 모델을 2번 돌리겠다.
+
+inputs = ak.ImageInput()
+outputs = ak.ImageBlock(
+    block_type='resnet',
+    normalize=True,
+    augment=False
+)(inputs)
+outputs = ak.ClassificationHead()(outputs)
+
+model = ak.AutoModel(
+    inputs = inputs, outputs=outputs, overwrite=True, max_trials=1
+)
 
 
 # 3. 훈련
 model.fit(x_train, y_train, epochs=2)
-
 
 # 4. 평가, 예측
 y_predict = model.predict(x_test)
---
+++ 02_keras2/keras76_autokeras4_Regressor.py
@@ -0,0 +1,20 @@
+from sklearn.datasets import load_boston
+import autokeras as ak
+import pandas as pd
+
+
+# 1. 데이터
+datasets = load_boston()
+x = datasets.data
+y = datasets.target
+
+
+# 2. 모델
+model = ak.StructureDataRegressor(overwrite=True, max_trials=1)
+
+
+# 3. 훈련
+model.fit(x, y, epochs=2, validation_split=0.2)
+
+# 4. 평가, 예측
+results = model.evaluate(x, y)
